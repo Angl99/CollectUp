179def7ca9e7a9d20ae06ee646018367fdc1d7a5
Angl99 <anggelplasencia@pursuit.org>
2024-07-22 14:22:42 -0400

    Created a new React component `GenerateItem` to handle the form for generating items.

---
+++ src/Components/Items/GenerateItem.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ItemDisplay from "./ItemDisplay"
+
+export default function GenerateItem() {
+    return (
+        <>
+            <form>
+                
+
+            </form>
+        </>
+    )
+}
