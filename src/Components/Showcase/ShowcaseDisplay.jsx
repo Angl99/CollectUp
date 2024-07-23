b7568974a0299333eb44332d230107c5e7aa1875
Angl99 <anggelplasencia@pursuit.org>
2024-07-23 17:21:35 -0400

    Removed unused ShowcaseItem component from ShowcaseDisplay.

--- src/Components/Showcase/ShowcaseDisplay.jsx
+++ src/Components/Showcase/ShowcaseDisplay.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
-import ShowcaseItem from "./ShowcaseItem";
 
 export default function ShowcaseDisplay() {
   const location = useLocation();
