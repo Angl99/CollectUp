05b1de6a798cc57da4dbab467c4e9080af78e389
Angl99 <anggelplasencia@pursuit.org>
2024-07-30 21:14:39 -0400

    prod changes

--- src/Components/firebase/firebaseConfig.jsx
+++ src/Components/firebase/firebaseConfig.jsx
@@ -4,7 +4,7 @@ import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-const firebaseKey = import.meta.env.VITE_FIREBASE_KEY
+const firebaseKey = import.meta.env.VITE_FIREBASE_PROD_KEY
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
